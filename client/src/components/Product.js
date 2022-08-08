2c2a13c526d04147f21aa3044f4c330c3b72bd8e
TienNguyen93 <nguyentien9933@gmail.com>
2022-08-08 13:38:05 -0400

    debug scrollable drop down & setting up state

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { FaStar } from 'react-icons/fa'
-import { Card, Image, Button, Container, Row, Col, Dropdown, ListGroup }
+import { Card, Image, Button, Container, Row, Col, Dropdown, ListGroup, DropdownButton }
     from 'react-bootstrap'
-import DropdownItem from "react-bootstrap/esm/DropdownItem";
 
+import { useState } from "react";
 
 
 const Product = ({ product }) => {
@@ -34,6 +34,17 @@ const Product = ({ product }) => {
             border: 'none'
         }
     }
+
+
+    // const quantity = {
+    //     unit: [...Array(10).keys()]
+    // }
+
+    const unit = [...Array(10).keys()]
+
+    const [units, setUnits] = useState(unit[0] + 1)
+
+    console.log('quant', units)
 
     return (
         // <div className="row" style={{ padding: '20px' }}>
@@ -73,8 +84,8 @@ const Product = ({ product }) => {
         // </div>
 
 
-        <Container style={{ padding: '20px' }}>
-            <Row style={{ alignItems: 'flex-start' }}>
+        <Container style={{ padding: '20px'}}>
+            <Row style={{ alignItems: 'flex-start', gap: '1rem' }}>
                 {/* <Col sm={8}> */}
                 <Col>
                     <Image fluid src={product?.image} />
@@ -96,33 +107,29 @@ const Product = ({ product }) => {
                 </Col>
 
                 <Col sm={4}>
-                    <Card>
+                    <Card style={{overflow: 'inherit'}}>
                         <Card.Body>
                             <Card.Text>
                                 Status: In Stock
                             </Card.Text>
 
-                            <Card.Text>
-                                <Dropdown>
-                                    <Dropdown.Toggle variant="success" id="dropdown-basic" style={{ borderRadius: '15px' }}>
-                                        Qty:
-                                    </Dropdown.Toggle>
+                            <Dropdown style={{ marginBottom: '1rem' }}>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic" style={{ borderRadius: '15px' }}>
+                                    Qty: {units}
+                                </Dropdown.Toggle>
+                                
+                                <Dropdown.Menu style={{maxHeight: '200px', overflowY: 'auto'}}>
+                                {unit.map(data => (
+                                        <Dropdown.Item key={data}>{data + 1}</Dropdown.Item>
+                                    ))}
+                                </Dropdown.Menu>
+                            </Dropdown>
 
-                                    <Dropdown.Menu>
-                                        {[...Array(5)].map(index => {
-                                            return (
-                                                <Dropdown.Item eventKey={index}>mull</Dropdown.Item>
-                                            )
-                                        })}
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Card.Text>
-
-                            <Card.Text>
+                            <div>
                                 <Button style={style.button}>
                                     Add to Cart
                                 </Button>
-                            </Card.Text>
+                            </div>
                         </Card.Body>
                     </Card>
                 </Col>
