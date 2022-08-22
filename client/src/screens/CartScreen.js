8aa9389f959b9739fbedb264e8d1dee35c693b2c
TienNguyen93 <nguyentien9933@gmail.com>
2022-08-22 13:37:59 -0400

    styling cartscreen

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -27,21 +27,59 @@ const CartScreen = ({ product }) => {
         <Container style={{ padding: '20px' }}>
             <Row>
                 <Col>
-                    <Card style={{ padding: '5px' }}>
+                    <Card style={{ padding: '10px' }}>
                         <div className="row">
                             <div className="col">
-                                <h2 style={{ paddingLeft: '10px' }}>Shopping Cart</h2>
+                                <h2>Shopping Cart</h2>
                             </div>
 
-
-                            <div className="col">
+                            <div className="col" style={{ textAlign: 'end' }}>
                                 <h5>Price</h5>
+                            </div>
+                        </div>
+
+                        <hr style={{ height: '0.5px' }} />
+
+                        <div className="row">
+                            <div className='col'>
+                                <Image fluid src="https://m.media-amazon.com/images/I/61x-NhdKBmL._AC_SX679_.jpg" />
+                            </div>
+
+                            <div className='col'>
+                                <Card.Title>
+                                    Amazon Basics Non-Stick Cookware Set, Pots, Pans and Utensils - 15-Piece Set
+                                </Card.Title>
+                                <div style={{ paddingBottom: '1rem' }}>
+                                    <Button style={style.dropdown}>
+                                        <label>Qty:</label>
+                                        <select style={{ border: 'none' }}>
+                                            {unit.map(u => (
+                                                <option key={u} value={u}>{u + 1}</option>
+                                            ))}
+                                        </select>
+                                    </Button>
+                                </div>
+                                <div>
+                                    <Button style={style.button}>
+                                        <FaTrash /> Delete
+                                    </Button>
+                                </div>
+                            </div>
+
+                            <div className='col' style={{ textAlign: 'end' }}>
+                                <h5 style={{ fontWeight: 'bold' }}>$72.47</h5>
                             </div>
 
                         </div>
 
+                        <hr />
 
-                        <Row style={{ alignItems: 'flex-start', gap: '1rem' }}>
+                        <div style={{ textAlign: 'end' }}>
+                            <h5>Subtotal (1 item): $72.47</h5>
+                        </div>
+
+
+                        {/* <Row style={{ alignItems: 'flex-start', gap: '1rem' }}>
                             <Col>
                                 <Image fluid src="https://m.media-amazon.com/images/I/61x-NhdKBmL._AC_SX679_.jpg" />
                             </Col>
@@ -68,7 +106,7 @@ const CartScreen = ({ product }) => {
                                     <h3>Subtotal (1 item): $72.47</h3>
                                 </Card.Body>
                             </Col>
-                        </Row>
+                        </Row> */}
 
 
                     </Card>
